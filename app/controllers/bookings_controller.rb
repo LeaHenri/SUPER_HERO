07232d8466043f93895e5b4e3edf70ce4ac41740
Lea Henri <lea-henri@live.fr>
2025-03-11 15:22:05 +0100

    show de hero, controller de booking

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,5 +1,19 @@
 class BookingsController < ApplicationController
   def create
-    # @hero = Hero.find(params[:hero_id])
+    @hero = Hero.find(params[:hero_id])
+    @booking = Booking.new(booking_params)
+    @booking.hero = @hero
+    @booking.user = current_user
+    if @booking.save
+      redirect_to hero_path(@hero)
+    else
+      render "heros/show", status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def booking_params
+    params.require(:booking).permit(:start_date, :end_date, :status)
   end
 end
